97b8d93d1d8f0c388054005cfe3480966ecc7769
Kazim Daqiqi <Kazim Daqiqi@Mohamma.mynetworksettings.com>
2024-01-07 14:54:49 -0500

    Exercise Two

---
+++ src/two/Two.java
@@ -0,0 +1,12 @@
+package two;
+
+public class Two {
+	public static void main(String [] args) {
+		System.out.println("King Kazim............");
+		System.out.println("Excercise Two.....");
+	}
+	// Add implementation....
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+}
